c381c7f4808b31f24f866876c36fca0cce2a5a09
Albert Dellor <dell.datascience@gmail.com>
2024-02-16 21:22:50 +0000

    correct commit for dlt project

--- week_2_workflow_orchestration/chapter_4/GcsBucket_Block.py
+++
@@ -1,7 +0,0 @@
-from prefect_gcp.cloud_storage import GcsBucket, GcpCredentials
-
-bucket_block = GcsBucket(gcp_credentials=GcpCredentials.load(name="gcp-credentials"),
-                bucket="de_data_lake_de-project-397922",
-                )
-
-bucket_block.save(name="zoom-gcs", overwrite=True)
